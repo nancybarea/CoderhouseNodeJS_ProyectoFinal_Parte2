b619f187bfc459d8e4be39d28e4b99eec9dd3c00
nancy <nancybarea@gmail.com>
2022-03-18 10:59:48 -0300

    salida de errores de castellano a ingles

--- src/controller/DefaultController.js
+++ src/controller/DefaultController.js
@@ -3,6 +3,6 @@ import logger from '../logger.js'
 export function failRoute(req, res) {
     const title = 'ROUTING ERROR';
     const { url, method } = req
-    logger.warn(`Ruta ${method} ${url} inexistente`)
+    logger.warn(`Route ${method} ${url} non-existent`)
     res.status(404).json( { titulo: title });
   }
--- src/model/daos/ContainerDao.js
+++ src/model/daos/ContainerDao.js
@@ -13,7 +13,7 @@ export default class ContainerDao {
     constructor(collection) {
         this.collectionName = collection
         this.collection = client.db(base).collection(collection)
-        logger.info(`Mongo Base:${base} collection: ${collection} instanciada`)
+        logger.info(`Mongo Base:${base} collection: ${collection} instantiated`)
     }
 
     async getAll() {
@@ -22,7 +22,7 @@ export default class ContainerDao {
             return array
         }
         catch (err) {
-            throw new CustomError(500, `error al obtener todos los registros de la coleccion ${this.collectionName}`, err)
+            throw new CustomError(500, `Error getting all records in collection ${this.collectionName}`, err)
         }
     }
 
@@ -34,11 +34,11 @@ export default class ContainerDao {
         }
         catch (err) {
             logger.error(err)
-            throw new CustomError(500, `error al obtener un Documento por codigo en la coleccion ${this.collectionName}`, err)
+            throw new CustomError(500, `Error when obtaining a Document by code in the collection ${this.collectionName}`, err)
         }
 
         if (!wanted) {
-            throw new CustomError(404, `Documento no encontrado con ese ${JSON.stringify(query)}`)
+            throw new CustomError(404, `Document not found with that ${JSON.stringify(query)}`)
         }
         return wanted
     }
@@ -51,11 +51,11 @@ export default class ContainerDao {
         }
         catch (err) {
             logger.error(err)
-            throw new CustomError(500, `error al obtener un Documento por codigo en la coleccion ${this.collectionName}`, err)
+            throw new CustomError(500, `Error when obtaining a Document by code in the collection ${this.collectionName}`, err)
         }
 
         if (!wanted) {
-            throw new CustomError(404, `Documento no encontrado con ese ${JSON.stringify(query)}`)
+            throw new CustomError(404, `Document not found with that ${JSON.stringify(query)}`)
         }
         return wanted
     }
@@ -66,7 +66,7 @@ export default class ContainerDao {
             return array
         }
         catch (err) {
-            throw new CustomError(500, `error al obtener todos los registros de la coleccion ${this.collectionName}`, err)
+            throw new CustomError(500, `Error getting all records in collection ${this.collectionName}`, err)
         }
 
     }
@@ -78,14 +78,14 @@ export default class ContainerDao {
             return insertedId;
         }
         catch (err) {
-            throw new CustomError(500, `error al Agregar un documento mongo en la coleccion ${this.collectionName}`, err)
+            throw new CustomError(500, `Error adding mongo document to collection ${this.collectionName}`, err)
         }
     }
 
     async deleteById(query) {
         await this.collection.deleteOne(query, function (err, obj) {
             if (err) {
-                throw new CustomError(500, `error al obtener todos los documento de la coleccion ${this.collectionName}`, err)
+                throw new CustomError(500, `Error getting all documents in collection ${this.collectionName}`, err)
             }
         });
     }
