348c86f2be15babdfed95c348c56c87b639cfb2c
nancy <nancybarea@gmail.com>
2022-03-18 09:43:23 -0300

    arreglo ruta movies y mas documentacion

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -4,11 +4,6 @@ import * as moviesController from "../controller/MoviesController.js";
 
 const MoviesRoute  = new Router();
 
-MoviesRoute.get("/movies", passport.authenticate('jwt', { session: false }), moviesController.getAll);
-
-// moviesController.get("/movies/:id", mwdIsAuth, moviesController.getMovie);
-// moviesController.post("/movies", mwdIsAuth, moviesController.postMovie);
-// moviesController.put("/movies/:id", mwdIsAuth, moviesController.putMovie);
-// moviesController.delete("/movies/:id", mwdIsAuth, moviesController.deleteMovie);
+MoviesRoute.get("/", passport.authenticate('jwt', { session: false }), moviesController.getAll);
 
 export default MoviesRoute;
